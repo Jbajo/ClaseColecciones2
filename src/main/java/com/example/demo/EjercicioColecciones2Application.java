9178f75860dd751e1acf92673ae26b9e41c475d2
Javier Bajo <javier.bajochacon@gmail.com>
2017-12-27 19:59:23 +0100

    Primer commit

---
+++ src/main/java/com/example/demo/EjercicioColecciones2Application.java
@@ -0,0 +1,64 @@
+package com.example.demo;
+
+import java.util.Hashtable;
+import java.util.Scanner;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EjercicioColecciones2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EjercicioColecciones2Application.class, args);
+		
+		TablaContactos tabla = new TablaContactos();
+		int opcion = 1;
+		String dni = new String();
+		String nombre = new String();
+		Scanner sc = new Scanner (System.in);
+		
+		do {
+			System.out.println("--------------------------");
+			System.out.println("Añadir un contacto --> (1)");
+			System.out.println("Eliminar un contacto --> (2)");
+			System.out.println("Mostrar contactos --> (3)");
+			System.out.println("Salir --> (0)");
+			System.out.println("--------------------------");
+			opcion = sc.nextInt();
+					
+			switch(opcion) {
+				case 1:
+					System.out.println("Introduzca un DNI");
+					dni=sc.next();
+					sc.nextLine();//limpiamos el buffer
+					System.out.println("Introduzca un nombre");
+					nombre=sc.nextLine();					
+					tabla.aniadirContacto(dni, nombre);
+					break;					
+				case 2:
+					System.out.println("Introduzca un DNI");					
+					dni=sc.next();
+					tabla.eliminarContacto(dni);
+					break;
+				case 3:
+					tabla.imprimirContactos();
+					break;
+				case 0:
+					System.out.println("Saliendo del programa....");
+					break;
+				default:
+					System.out.println("Opcion no valida...");
+					break;
+					
+			}			
+			
+		}while(opcion!=0);
+		
+		
+		
+		
+		
+		
+	}
+}
---
+++ src/main/java/com/example/demo/TablaContactos.java
@@ -0,0 +1,54 @@
+package com.example.demo;
+
+import java.util.Hashtable;
+/**
+ * 
+ * @author Javier Bajo Chacon  javier.bajochacon@gmail.com
+ *
+ */
+public class TablaContactos {
+	
+	private Hashtable<String,String> listaContactos;
+/**
+ * Constructor de la clase TablaContactos
+ */
+	public TablaContactos() {
+		super();
+		this.listaContactos = new Hashtable<String, String>();
+	}
+	
+	/**
+	 * 
+	 * @param dni dni del usuario a añadir
+	 * @param nombre nombre del usuario a añadir
+	 */
+	public void aniadirContacto(String dni, String nombre) {
+		if(!this.listaContactos.contains(dni)){
+			this.listaContactos.put(dni, nombre);	
+			
+		}
+		
+	}
+	
+	/**
+	 * 
+	 * @param dni dni del usuario a eliminar
+	 */
+	
+	public void eliminarContacto(String dni) {
+		if(this.listaContactos.containsKey(dni)) {
+			this.listaContactos.remove(dni);
+		}
+	}
+	
+	/**
+	 * método que imprime los contactos de la listaContactos 
+	 */
+	
+	public void imprimirContactos() {
+		this.listaContactos.forEach((k,v)->System.out.println("El contacto con DNI --> " + k + " tiene el nombre --> " + v));
+	}
+	
+	
+
+}
