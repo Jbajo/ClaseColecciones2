d98ae187c4f106d2a1650f7566788deb7b815463
jbajo <javier.bajochacon@gmail.com>
2018-01-20 12:11:30 +0100

    Commit validacion DNI

--- src/main/java/com/cice/modelo/TablaContactos.java
+++ src/main/java/com/cice/modelo/TablaContactos.java
@@ -1,10 +1,8 @@
 package com.cice.modelo;
 
-import java.util.ArrayList;
 import java.util.Enumeration;
 import java.util.Hashtable;
 import java.util.Iterator;
-import java.util.List;
 import java.util.Set;
 /**
  * 
